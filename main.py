7416cbcc0f5398eaf986beb764c1c247f2425ea2
Fallods <ericc404@nycstudents.net>
2024-02-07 23:55:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+def DigitSum(n):
+    sum = 0
+    for digit in str(n):
+        sum += int(digit)
+    return sum
+
+numbers = []
+x = int(input('What is your lower bound?'))
+y = int(input('What is your higher bound?'))
+digitsum = int(input('What digit sum should your numbers sum to?'))
+for i in range(x,y):
+    a = DigitSum(i)
+    if(a == digitsum):
+        #print(i)
+        numbers.append(int(i))
+
+
+print(f'You have {len(numbers)} digit(s) with a digit sum of {digitsum}')
+print(f'Here is your list: {numbers}')
+
+
+
+
 
